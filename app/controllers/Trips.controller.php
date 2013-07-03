905ba754d1456ebf5ef8794adf745ac4ec3c53c0
Hauke Schade <cnlpete@cnlpete.de>
2013-07-03 15:15:55 +0200

    added license

--- app/controllers/Trips.controller.php
+++ app/controllers/Trips.controller.php
@@ -1,5 +1,13 @@
 <?php
+/**
+ * Copyright (C) Hauke Schade, 2013
 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+**/
 namespace CandyCMS\Controllers;
 
 use CandyCMS\Core\Helpers\Helper;
